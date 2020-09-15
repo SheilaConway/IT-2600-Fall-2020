0db315a40d84e3e3f9bbf06e4c370f26708a694d
SheilaConway <31965081+SheilaConway@users.noreply.github.com>
2020-09-14 23:25:36 -0400

    Add files via upload

---
+++ action1.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+
+Your number is <?php echo $_POST["int"]; ?><br>
+<b>
+<?php
+echo "The value entered was: <br>";
+echo "The square root:";
+echo '<br>';
+echo "The square:<br>";
+echo "The factorial:<br>";
+?>
+</b>
+<a href="http://localhost/andrew.php"> "Click here to return to the input form."</a>
+</body>
+</html> 
